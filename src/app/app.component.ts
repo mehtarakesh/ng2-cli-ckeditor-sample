edc22c6f9551acd042be87030e0ea79b840e1269
Simon Babay <simon@babay.fr>
2016-07-06 01:07:43 +0200

    Tests with new forms api

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,34 @@
 import { Component } from '@angular/core';
 import { CKEditor } from 'ng2-ckeditor';
+import { REACTIVE_FORM_DIRECTIVES, FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   moduleId: module.id,
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.css'],
-  directives: [CKEditor],
+  directives: [REACTIVE_FORM_DIRECTIVES, CKEditor],
 })
 export class AppComponent {
-  title = 'app works!';
-  data = 'I love <strong>HTML</strong>';
+
+  modelForm:FormGroup;
+
+  constructor(private formBuilder:FormBuilder){
+  }
+
+  ngOnInit(){
+    this.modelForm = this.formBuilder.group({
+      title: ['', Validators.required],
+      content: ['', Validators.required],
+    });
+  }
+
+  onSubmitModelForm(form){
+    console.log(form);
+  }
+
+  onSubmitTemplateForm(form){
+    console.log(form);
+  }
+
 }
